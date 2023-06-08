bb6f7281a332571d552524a5b4a3a93a651f80b3
Rupesh choudhary <rupeshchoudhary1212@gmail.com>
2023-06-08 11:30:27 +0530

    second commit

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -1,6 +1,6 @@
 import React,{useRef,useState} from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import '../hero/Swip.css'
+import "../hero/Swip.css"
 
 import 'swiper/css';
 import 'swiper/css/pagination';
